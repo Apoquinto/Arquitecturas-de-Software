5108b0ba95036d3c49c22ce6a303e6a1c50c3576
Apoquinto <RCLlanesA@gmail.com>
2021-09-20 01:47:52 -0500

    :coffin: Remove deprecated main

--- ADA6/presentation/Main.java
+++
@@ -1,9 +0,0 @@
-package presentation;
-
-public class Main{
-
-    public static void main(String[] args) {
-        PresentationModule presentationLayer = new PresentationModule(new ColorConfiguration());
-        presentationLayer.run();
-    }
-}
