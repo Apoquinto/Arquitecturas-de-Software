4a14f5598b0b6c25416e5b4ecff264116d702cb2
Apoquinto <RCLlanesA@gmail.com>
2021-10-06 16:40:39 -0500

    :sparkles: Add event class

--- Ada_09/project/src/model/Event.java
+++ Ada_09/project/src/model/Event.java
@@ -1,2 +1,20 @@
-package model;public class Event {
+package model;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private String event;
+    private String message;
+    private LocalDateTime datetime;
+
+    public Event(String event, String message){
+        this.event = event;
+        this.message = message;
+        this.datetime = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "[ " + event + " ] " + message + " a las " + datetime.toString();
+    }
 }
